8e4d52d5cecb1f74365b0641e394e4f3843da535
Ahmed Hamza Aydin <ahamzaaydin.py@gmail.com>
2022-07-30 11:56:45 +0200

    Change settings variable usages

--- backend/users/utils/email.py
+++ backend/users/utils/email.py
@@ -19,14 +19,17 @@ class EmailThread(threading.Thread):
         self.email_message.send()
 
 def send_activation_email(request, user: User):
+    uid = urlsafe_base64_encode(force_bytes(user.pk))
+    token = token_generator.make_token(user)
+    
+    activation_url = settings.USER['ACTIVATION_URL'].replace(':uid', uid).replace(':token', token)
+    
     current_site = get_current_site(request)
     email_subject = 'Activate your blogger account'
     message = render_to_string('users/email/activate.html', {
         'user': user,
         'domain': current_site.domain,
-        'activation_url': settings.USERS['ACTIVATION_URL'],
-        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-        'token': token_generator.make_token(user)
+        'activation_url': activation_url
     })
     
     email_message = EmailMessage(
@@ -45,14 +48,17 @@ def send_password_reset_email(request, email: str):
     except User.DoesNotExist:
         raise User.DoesNotExist()
     
+    uid = urlsafe_base64_encode(force_bytes(user.pk))
+    token = token_generator.make_token(user)
+    
     current_site = get_current_site(request)
     email_subject = 'Reset your password'
+    password_reset_url = settings.USERS['PASSWORD_RESET_URL'].replace(':uid', uid).replace(':token', token)
+    
     message = render_to_string('users/email/password_reset.html', {
         'user': user,
         'domain': current_site.domain,
-        'password_reset_url': settings.USERS['PASSWORD_RESET_URL'],
-        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-        'token': token_generator.make_token(user)
+        'password_reset_url': password_reset_url
     })
     
     email_message = EmailMessage(
